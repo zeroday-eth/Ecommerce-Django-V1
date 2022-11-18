8f8e741e29be9e1302c7c4a131aac0e20e78bf0e
zeroday-eth <81301466+zeroday-eth@users.noreply.github.com>
2022-11-18 14:11:01 +0300

    This project was developed using the Django module.
    Used XAMPP and mysql.
    
    python -m pip install pillow
    
    python manage.py makemigrations
    
    py manage.py migrate
    
    python -m pip install mysql-connector-python
    
    py manage.py runserver

---
+++ DjangoEcom/ecommerce/store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path 
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('collections', views.collections, name="collections"),
+    path('collections/<str:slug>', views.collectionsview, name="collectionsview"),
+    path('collections/<str:cate_slug>/<str:prod_slug>', views.productview, name="productview"),
+
+    path('product-list', views.productlistAjax),
+    path('searchproduct', views.searchproduct, name="searchproduct"),
+]
